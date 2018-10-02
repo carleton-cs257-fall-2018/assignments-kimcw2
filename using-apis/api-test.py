036ad3d7c40a447be49c753ac763330838a393b2
kimcw2 <31895534+kimcw2@users.noreply.github.com>
2018-10-01 21:31:11 -0700

    small error msg fix

--- using-apis/api-test.py
+++ using-apis/api-test.py
@@ -7,7 +7,7 @@ import sys
 
 if len(sys.argv) > 0:
     print("\nUsage: This program allows user to search artists and take the corresponding ID number to gain more information")
-    print("No need to put in any arguments.", file = sys.stderr)
+    print("No need to put in any arguments.\n", file = sys.stderr)
 
 print("Please answer the following questions")
 

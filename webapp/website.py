9116e230a0fb3e6bd0e6b219ac54e9ddc5c09af9
Your Name <dalmeidad@carleton.edu>
2018-10-31 01:29:02 -0500

    test

--- webapp/website.py
+++ webapp/website.py
@@ -22,13 +22,13 @@ def get_main_page():
 @app.route('/about')
 def get_about_page():
     ''' This is the only route intended for human users '''
-    global api_port
+    global port
     return flask.render_template('about.html', api_port=api_port)
 
 @app.route('/submit_search')
 def get_submit_search_page():
     ''' This is the only route intended for human users '''
-    global api_port
+    global port
     return flask.render_template('submit_search.html', api_port=api_port)
 
 if __name__ == '__main__':

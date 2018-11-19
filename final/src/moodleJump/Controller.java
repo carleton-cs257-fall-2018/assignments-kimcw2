35743964034af7fa4f35d3e0e5cc3d727c5309a5
Your Name <dalmeidad@carleton.edu>
2018-11-19 04:53:33 -0600

    Updates to our game - generally structuring code

--- final/src/moodleJump/Controller.java
+++ final/src/moodleJump/Controller.java
@@ -6,7 +6,101 @@ import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
-public class Controller{
+public class Controller implements EventHandler<KeyEvent>{
+    @FXML private Label scoreLabel;
+    @FXML private Label messageLabel;
+    @FXML private GameView gameView;
+
+    private Thread thread;
+    private boolean running = false;
+
+    private Model model;
+
+    public Controller() {
+    }
+
+    public void initialize() {
+        this.model = new Model(this.gameView.getRowCount(), this.gameView.getColumnCount());
+        this.update();
+    }
+
+    private void update() {
+        this.gameView.update(this.model);
+    }
+
+    public double getBoardWidth() {
+        return GameView.CELL_WIDTH * this.gameView.getColumnCount();
+    }
+
+    public double getBoardHeight() {
+        return GameView.CELL_WIDTH * this.gameView.getRowCount();
+    }
+
+    @Override
+    public void handle(KeyEvent keyEvent) {
+        boolean keyRecognized = true;
+        KeyCode code = keyEvent.getCode();
+
+        if (code == KeyCode.LEFT || code == KeyCode.A) {
+            this.model.moveMoodler("left");
+        } else if (code == KeyCode.RIGHT || code == KeyCode.D) {
+            this.model.moveMoodler("right");
+        } else {
+            keyRecognized = false;
+        }
+        /*else if (code == KeyCode.UP || code == KeyCode.W) {
+            this.daleksModel.moveRunnerBy(-1, 0);
+        } else if (code == KeyCode.DOWN || code == KeyCode.X) {
+        */
+        if (keyRecognized) {
+            //System.out.print("key recognized\n");
+            this.update();
+            keyEvent.consume();
+        }
+    }
+    /*
+    public void start() {
+        thread = new Thread(this);
+        thread.start();
+        running = true;
+    }
+
+    public void stop() {
+        try {
+            thread.join();
+            running = false;
+        }catch(exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        long lastTime = System.nanoTime();
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while(running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            while (delta >= 1 ) {
+                tick();
+                delta--;
+            }
+            if (running) {
+                render();
+            }
+            frames++;
+
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+        stop();
+    }*/
+
 
     /**
      * Pops up start page
--- final/src/moodleJump/Main.java
+++ final/src/moodleJump/Main.java
@@ -6,10 +6,36 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main{
+
+
+public class Main extends Application {
     /**
      * Initiates controller
      */
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("moodleJump.fxml"));
+        Parent root = loader.load();
+
+        primaryStage.setTitle("Moodle Jump");
+
+
+        Controller controller = loader.getController();
+        root.setOnKeyPressed(controller);
+        double sceneWidth = controller.getBoardWidth() + 20.0;
+        double sceneHeight = controller.getBoardHeight() + 100.0;
+
+
+        primaryStage.setScene(new Scene(root, sceneWidth, sceneHeight));
+        primaryStage.show();
+        //controller.start();
+        root.requestFocus();
+    }
+
+
     public static void main(String[] args) {
+        launch(args);
     }
 }
